4b5c13b5bcce860cd4570cc4bf2d545a45381314
Vitaly-R <vitaly.rajev@gmail.com>
2019-10-19 09:59:24 +0300

    Upload game files

---
+++ Game.py
@@ -0,0 +1,256 @@
+import pygame
+import random
+
+
+class Game:
+    __O_WIDTH = 10
+    __B_WIDTH = 3
+    __S_WIDTH = 100 * __O_WIDTH
+    __S_HEIGHT = 60 * __O_WIDTH
+    __M_HEIGHT = 40
+
+    __U = [0, -__O_WIDTH]
+    __D = [0, __O_WIDTH]
+    __L = [-__O_WIDTH, 0]
+    __R = [__O_WIDTH, 0]
+
+    __FONT = 'David MS'
+    __SIZE = 30
+    __SCORE = 'Score: '
+    __INSTRUCTIONS = '\'p\'-Pause | \'Esc\'-Exit'
+    __PAUSE = 'Game paused'
+    __CONT = 'Press \'c\' to continue'
+    __GAME_OVER_MSG = 'GAME OVER!'
+    __NEW_GAME_MSG = 'Press \'n\' to play again'
+    __QUIT_GAME_MSG = 'Press \'Esc\' to exit'
+
+    def __init__(self):
+        self.__init_game()
+        self.__init_values()
+        self.__init_colors()
+
+    def __init_values(self):
+        self.__direction = self.__U
+        self.__snake = [[self.__S_WIDTH // 2, self.__S_HEIGHT // 2]]
+        self.__foods = list()
+        self.__counter = 0
+        self.__score = 0
+        self.__running = True
+        self.__start = True
+        self.__end_game = False
+        self.__pause = False
+        self.__refresh_rate = 15
+        self.__food_frequency = 29
+        self.__max_food = 5
+
+    def __init_game(self):
+        pygame.init()
+        pygame.font.init()
+        self.__screen = pygame.display.set_mode((self.__S_WIDTH, self.__S_HEIGHT + self.__M_HEIGHT))
+        self.__font = pygame.font.SysFont(self.__FONT, self.__SIZE, True)
+        self.__clock = pygame.time.Clock()
+
+    def __init_colors(self):
+        self.__def_color = (255, 255, 255)
+        self.__bg_color = (0, 0, 0)
+        self.__snake_color = (255, 255, 255)
+        self.__food_color = (0, 255, 0)
+        self.__text_color = (255, 255, 255)
+
+    def run(self):
+        while self.__running:
+            self.__run_loop()
+        self.__quit()
+
+    def __run_loop(self):
+        if self.__start:
+            self.__start_screen()
+        elif self.__end_game:
+            self.__end_loop()
+        elif self.__pause:
+            self.__pause_loop()
+        else:
+            self.__game_screen()
+
+    def __pause_loop(self):
+        while self.__pause:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__pause = False
+                    self.__end_game = True
+                elif event.type == pygame.KEYUP and event.key == pygame.K_c:
+                    self.__pause = False
+
+    def __start_screen(self):
+        while self.__start:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__start = False
+                    self.__running = False
+                if event.type == pygame.KEYUP and event.key == pygame.K_s:
+                    self.__start = False
+            text = self.__font.render('Press \'s\' to start game', False, (255, 255, 255))
+            self.__screen.blit(text, (self.__S_WIDTH // 3, self.__S_HEIGHT // 5))
+            pygame.display.flip()
+
+    def __end_loop(self):
+        while self.__end_game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__running = False
+                    self.__end_game = False
+                elif event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+                    self.__running = False
+                    self.__end_game = False
+                elif event.type == pygame.KEYUP and event.key == pygame.K_n:
+                    self.__end_game = False
+                    self.__snake = [[self.__S_WIDTH // 2, self.__S_HEIGHT // 2]]
+                    self.__foods = list()
+
+    def __game_screen(self):
+        self.__check_quit_event()
+        if self.__running:
+            self.__game_loop()
+
+    def __game_loop(self):
+        self.__screen.fill(self.__bg_color)
+        self.__handle_food()
+        self.__handle_keys()
+        self.__handle_movement()
+        self.__draw()
+        self.__update()
+
+    def __check_quit_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.__running = False
+
+    def __handle_food(self):
+        if (self.__counter % self.__food_frequency) == 0:
+            if len(self.__foods) < self.__max_food:
+                self.__make_food()
+
+    def __make_food(self):
+        pos = self.__make_food_pos()
+        while pos in self.__snake or pos in self.__foods:
+            pos = self.__make_food_pos()
+        self.__foods.append(pos)
+
+    def __make_food_pos(self):
+        x = random.randint(0, self.__S_WIDTH // self.__O_WIDTH) * self.__O_WIDTH
+        y = random.randint(0, self.__S_HEIGHT // self.__O_WIDTH - 1) * self.__O_WIDTH
+        return [x, y]
+
+    def __handle_keys(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_ESCAPE]:
+            self.__running = False
+        elif pressed[pygame.K_p]:
+            self.__pause_screen()
+        elif pressed[pygame.K_UP]:
+            self.__move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.__move_down()
+        elif pressed[pygame.K_LEFT]:
+            self.__move_left()
+        elif pressed[pygame.K_RIGHT]:
+            self.__move_right()
+
+    def __pause_screen(self):
+        self.__screen.blit(self.__font.render(self.__PAUSE, False, self.__text_color),
+                           (self.__S_WIDTH // 2.5, self.__S_HEIGHT // 5))
+        self.__screen.blit(self.__font.render(self.__CONT, False, self.__text_color),
+                           (self.__S_WIDTH // 2.75, self.__S_HEIGHT // 4))
+        self.__pause = True
+
+    def __move_up(self):
+        if self.__direction not in [self.__U, self.__D]:
+            self.__direction = self.__U
+
+    def __move_down(self):
+        if self.__direction not in [self.__U, self.__D]:
+            self.__direction = self.__D
+
+    def __move_left(self):
+        if self.__direction not in [self.__R, self.__L]:
+            self.__direction = self.__L
+
+    def __move_right(self):
+        if self.__direction not in [self.__R, self.__L]:
+            self.__direction = self.__R
+
+    def __handle_movement(self):
+        if self.__check_eat():
+            self.__eat()
+        elif self.__check_collisions():
+            self.__game_over_screen()
+        else:
+            self.__move_snake()
+
+    def __check_eat(self):
+        return [self.__snake[0][0] + self.__direction[0], self.__snake[0][1] + self.__direction[1]] in self.__foods
+
+    def __eat(self):
+        self.__snake.insert(0, self.__foods.pop(self.__foods.index([self.__snake[0][0] + self.__direction[0],
+                                                                    self.__snake[0][1] + self.__direction[1]])))
+        self.__score += 1
+
+    def __check_collisions(self):
+        return self.__snake[0] in self.__snake[1:] or \
+               (self.__check_vertical_collision() or self.__check_horizontal_collision())
+
+    def __check_vertical_collision(self):
+        return not (0 <= (self.__snake[0][1] + self.__direction[1]) <= (self.__S_HEIGHT - self.__O_WIDTH))
+
+    def __check_horizontal_collision(self):
+        return not (0 <= (self.__snake[0][0] + self.__direction[0]) <= (self.__S_WIDTH - self.__O_WIDTH))
+
+    def __game_over_screen(self):
+        self.__screen.blit(self.__font.render(self.__GAME_OVER_MSG, False, self.__text_color),
+                           (self.__S_WIDTH // 2.5, self.__S_HEIGHT // 5))
+        self.__screen.blit(self.__font.render(self.__NEW_GAME_MSG, False, self.__text_color),
+                           (self.__S_WIDTH // 3, self.__S_HEIGHT // 4))
+        self.__screen.blit(self.__font.render(self.__QUIT_GAME_MSG, False, self.__text_color),
+                           (self.__S_WIDTH // 2.75, self.__S_HEIGHT // 3.5))
+        self.__end_game = True
+
+    def __move_snake(self):
+        if len(self.__snake) == 1:
+            self.__snake[0][0] += self.__direction[0]
+            self.__snake[0][1] += self.__direction[1]
+        else:
+            self.__snake.pop()
+            self.__snake.insert(0, [self.__snake[0][0] + self.__direction[0], self.__snake[0][1] + self.__direction[1]])
+
+    def __draw(self):
+        self.__draw_sub_menu()
+        self.__draw_snake()
+        self.__draw_food()
+
+    def __draw_sub_menu(self):
+        pygame.draw.rect(self.__screen, self.__def_color,
+                         pygame.Rect(0, self.__S_HEIGHT, self.__S_WIDTH, self.__B_WIDTH))
+        self.__screen.blit(self.__font.render(self.__SCORE + str(self.__score), False, self.__text_color),
+                           (0, self.__S_HEIGHT + self.__O_WIDTH))
+        self.__screen.blit(self.__font.render(self.__INSTRUCTIONS, False, self.__text_color),
+                           (self.__S_WIDTH // (3 / 2.25), self.__S_HEIGHT + self.__O_WIDTH))
+
+    def __draw_snake(self):
+        for pos in self.__snake:
+            pygame.draw.rect(self.__screen, self.__snake_color,
+                             pygame.Rect(pos[0], pos[1], self.__O_WIDTH, self.__O_WIDTH))
+
+    def __draw_food(self):
+        for food in self.__foods:
+            pygame.draw.rect(self.__screen, self.__food_color,
+                             pygame.Rect(food[0], food[1], self.__O_WIDTH, self.__O_WIDTH))
+
+    def __update(self):
+        pygame.display.flip()
+        self.__counter += 1
+        self.__clock.tick(self.__refresh_rate)
+
+    @staticmethod
+    def __quit():
+        pygame.font.quit()
+        pygame.quit()
